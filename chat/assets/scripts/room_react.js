e48e7f67f5aa1942bf76843b47ad2d1dc4d600e5
Vishnu Krishnathu <vishnu.krishnathu1999@gmail.com>
2022-01-31 18:37:17 +0530

    React chatting system added

--- chat/assets/scripts/room_react.js
+++ chat/assets/scripts/room_react.js
@@ -1,19 +1,66 @@
 'use strict';
-function LikeButton(){
-    const [messages, setMessages] = new React.useState([
+
+function ChatMessages({props}){
+    return (
+        <div className={props.type}>{props.message}</div>
+    )
+}
+
+function ChatBox(){
+    const [messages, setMessages] = React.useState([
         {type: 'sender', message: 'Hey Vishnu'},
         {type: 'receiver', message: 'Hi seller'},
         {type: 'sender', message: 'Can I get som more info on the product'},
         {type: 'receiver', message: 'Sure :-)'}
     ]);
+
+    const textMessage = React.useRef(null);
+    const buttonElement = React.useRef(null);
+
+    const [chatSocket, setChatSocket] = React.useState(() => {
+        let room_name = JSON.parse(document.getElementById('room-name').textContent);
+        return new WebSocket(`ws://${window.location.host}/ws/chat/${room_name}/`);
+    })
+
+    React.useEffect(() => {
+        chatSocket.onmessage = function(e){
+            let data = JSON.parse(e.data);
+            setMessages((message) => [...message, {type: 'receiver', message: data.message}]);
+        }
+
+        chatSocket.onclose = function(e){
+            console.error('Chat socket closed unexpectedly');
+        }
+    }, [chatSocket])
+
+    function sendMessage(){
+        chatSocket.send(
+            JSON.stringify({
+                'message': textMessage.current.value
+            })
+        )
+        textMessage.current.value = "";
+    }
+
+    function handleKey(e){
+        if(e.keyCode == 13){
+            buttonElement.current.click();
+        }
+    }
+
+
     return (
     <div>
-        {messages.map((text) => (
-            <div class={text.type}>{text.message}</div>
-        ))}
+        <div id="chat-log">
+            {messages.map(text => <ChatMessages props={text}/>)}
+        </div>
+        <div id="input-elements">
+            <input id="chat-message-input" type="text" ref={textMessage} onKeyUp={handleKey}/>
+            <button id="chat-message-submit" onClick={sendMessage} ref={buttonElement}>Send</button>
+        </div>
     </div>
     )
 }
 
-const domContainer = document.querySelector('#chat-log');
-ReactDOM.render(<LikeButton/>, domContainer);
+const domContainer = document.querySelector('#root');
+ReactDOM.render(<ChatBox/>, domContainer);
