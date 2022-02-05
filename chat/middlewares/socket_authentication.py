47cd06101f3a2d7d17fdb3936fa9af2402694044
Vishnu Krishnathu <vishnu.krishnathu1999@gmail.com>
2022-02-05 11:02:57 +0530

    new file added

---
+++ chat/middlewares/socket_authentication.py
@@ -0,0 +1,11 @@
+class SocketJWTAuthentication:
+    def __init__(self, app):
+        # Store the ASGI application we were passed
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        # Look up user from query string (you should also do things like
+        # checking if it is a valid user ID, or if scope["user"] is already
+        # populated).
+        return await self.app(scope, receive, send)
+
